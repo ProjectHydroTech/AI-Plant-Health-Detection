d41268b9e566b634f215ace4a8b5a874c5e39ba9
HydroTech <92703672+ProjectHydroTech@users.noreply.github.com>
2022-01-05 17:04:01 +0800

    Add files via upload

---
+++ firebase.py
@@ -0,0 +1,116 @@
+import pyrebase
+import os
+import shutil
+from pathlib import Path
+import glob
+import cv2
+import argparse
+from libraries.inference import detection_model, category_index
+import libraries.inference as infer
+
+#----------------------------------------------------------#
+#----------------Firebase auth-----------------------------#   
+
+config = {
+  'apiKey': "AIzaSyD_jFWX6SrZq-YzIrPIgyJaTIXHTj0vC6k",
+  'authDomain': "leaf-detection-74f49.firebaseapp.com",
+  'databaseURL': "https://leaf-detection-74f49-default-rtdb.asia-southeast1.firebasedatabase.app",
+  'projectId': "leaf-detection-74f49",
+  'storageBucket': "leaf-detection-74f49.appspot.com",
+  'messagingSenderId': "328009889613",
+  'appId': "1:328009889613:web:a00402855825110b08e904",
+  'measurementId': "G-F2V3W8VQKS",
+  'serviceAccount': "key.json"
+}
+
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+storage = firebase.storage()
+
+auth = firebase.auth()
+user = auth.sign_in_with_email_and_password("test@gmail.com", "123456789")
+
+#----------------------------------------------------------#
+#----------------Paths/Variables---------------------------#   
+
+datasetpath = "dataset"
+cache = ".cache"
+
+scorethresh = 0.8
+numberboxes = 20
+
+#----------------------------------------------------------#
+#----------------Functions---------------------------------#   
+    
+def getimgpaths(datasetpaths): #gets all images in directory and concantenates to an array
+
+    imagepaths = []
+    for file in glob.glob(os.path.join(datasetpaths,"*.jpg")) :
+        imagepaths.append(file)
+    for file in glob.glob(os.path.join(datasetpaths,"*.png")) :
+        imagepaths.append(file)
+    for file in glob.glob(os.path.join(datasetpaths,"*.jpeg")) :
+        imagepaths.append(file)
+
+    print("{} Images Found! {}".format(len(imagepaths), imagepaths))
+    return imagepaths
+
+def getcachedimages(imagename): #gets all images in directory and concantenates to an array
+    
+    cachedcroppedpaths = []
+    for file in glob.glob(os.path.join(cache,"{}_*_cropped.jpg".format(imagename))) :
+        cachedcroppedpaths.append(file)
+    num_cropcache = len(cachedcroppedpaths)
+    
+    return cachedcroppedpaths, num_cropcache
+
+#----------------------------------------------------------#
+#----------------Initialising Functions--------------------#
+
+try:
+    path = Path(cache) #initialise image .cache for uploading
+    path.mkdir(parents=True, exist_ok=False)
+except FileExistsError:
+    print(".cache already exists! removing now")
+    shutil.rmtree(cache) #removes image .cache
+    path = Path(cache) #initialise image .cache for uploading
+    path.mkdir(parents=True, exist_ok=True)
+    pass
+
+imgpaths = getimgpaths(datasetpath)
+num_images = len(imgpaths)
+
+for x in range (0, num_images) :
+    image_np = infer.image_load(imgpaths[x])
+    detected, image_with_detections = infer.image_detect(image_np, numberboxes, scorethresh, category_index, detection_model)
+    infer.image_crop(imgpaths[x], cache, scorethresh, detected)
+    
+    #remove front path and leave only image string (tail)
+    #i.e. cropped\\image.jpg --> image.jpg
+    head, sep, tail = imgpaths[x].partition('\\')
+
+    #remove '.jpg' or '.png' and leave only image name (head1)
+    #i.e. image.jpg --> image
+    head1, sep1, tail1 = tail.partition('.')
+
+    cv2.imwrite(os.path.join(cache,'{}_boxes.jpg'.format(head1)), image_with_detections) #write to .cache directory before uploading
+    #storage.delete('boundbox/') #clear tempbb directory before writing new image    
+    storage.child('boundbox').child('tempbb').put(os.path.join(cache,'{}_boxes.jpg'.format(head1))) #write to tempbb directory
+    storage.child('storage').child('imagewithboundbox').child('{}_boxes.jpg'.format(head1)).put(
+        os.path.join(cache,'{}_boxes.jpg'.format(head1))) #write to storage directory
+    
+    print(os.path.join(cache,'{}_boxes.jpg uploaded!'.format(head1)))
+    
+    cachedcroppedpaths, num_cropcache = getcachedimages(head1)
+    for y in range (0, num_cropcache):
+                          
+        header, seperator, tailname = cachedcroppedpaths[y].partition('\\') 
+        #storage.delete('cropped/') #clear tempcrop directory before writing new image
+        storage.child('cropped').child('tempcrop{}'.format(y)).put(cachedcroppedpaths[y]) #write to tempcrop directory
+        storage.child('storage').child('imagecropped').child(tailname).put(cachedcroppedpaths[y]) #write to storage directory
+        print(tailname, " uploaded!")
+        
+    print("#################################################")
+
+shutil.rmtree(cache) #removes image .cache
---
+++ run.py
@@ -0,0 +1,85 @@
+import os
+import glob
+import cv2
+import argparse
+from libraries.inference import detection_model, category_index
+import libraries.inference as infer
+ 
+# Initialize parser
+parser = argparse.ArgumentParser()
+ 
+# Adding arguments
+parser.add_argument("-d", "--dataset", help = "Image Dataset Path")
+parser.add_argument("-c", "--cropped", help = "Cropped Image Path")
+parser.add_argument("-b", "--boundbox", help = "Image + Bounding Box Path")
+parser.add_argument("-s", "--scorethresh", help = "Score Threshold")
+parser.add_argument("-nb", "--boundboxnumber", help = "Number of Bounding Boxes")
+
+# Read arguments from command line
+args = parser.parse_args()
+
+#Show directories
+if args.dataset:
+    datasetpath = args.dataset
+    print("Dataset path is: % s" % datasetpath)
+    
+if args.cropped:
+    croppedpath = args.cropped
+    print("Cropped path is: % s" % croppedpath)
+    
+if args.boundbox:
+    boundboxpath = args.boundbox
+    print("Image + Bound Box Path is: % s" % boundboxpath)
+
+if args.scorethresh is None:
+    scorethresh = 0.8
+    print("Using Default Score Threshold : % s" % scorethresh)
+else:
+    scorethresh = args.scorethresh
+    print("Using Set Score Threshold : % s" % scorethresh)
+
+if args.boundboxnumber is None:
+    numberboxes = 20
+    print("Using Default Number of Boxes : % s" % numberboxes)
+else:
+    numberboxes = args.scorethresh
+    print("Using Set Score Threshold : % s" % numberboxes)
+    
+#----------------------------------------------------------#
+#----------------Functions---------------------------------#   
+    
+def getimgpaths(datasetpaths):
+
+    imagepaths = []
+    for file in glob.glob(os.path.join(datasetpaths,"*.jpg")) :
+        imagepaths.append(file)
+    for file in glob.glob(os.path.join(datasetpaths,"*.png")) :
+        imagepaths.append(file)
+    for file in glob.glob(os.path.join(datasetpaths,"*.jpeg")) :
+        imagepaths.append(file)
+
+    print("{} Images Found! {}".format(len(imagepaths), imagepaths))
+    return imagepaths
+
+#----------------------------------------------------------#
+#----------------Initialising Functions--------------------#
+
+imgpaths = getimgpaths(datasetpath)
+num_images = len(imgpaths)
+
+for x in range (0, num_images) :
+    image_np = infer.image_load(imgpaths[x])
+    detected, image_with_detections = infer.image_detect(image_np, numberboxes, scorethresh, category_index, detection_model)
+    infer.image_crop(imgpaths[x], croppedpath, scorethresh, detected)
+    
+    #remove front path and leave only image string (tail)
+    #i.e. cropped\\image.jpg --> image.jpg
+    head, sep, tail = imgpaths[x].partition('\\')
+
+    #remove '.jpg' or '.png' and leave only image name (head1)
+    #i.e. image.jpg --> image
+    head1, sep1, tail1 = tail.partition('.')
+
+    cv2.imwrite(os.path.join(boundboxpath,'{}_boxes.jpg'.format(head1)), image_with_detections)
+    print(os.path.join(boundboxpath,'{}_boxes.jpg written!'.format(head1)))
+    print("#################################################")
